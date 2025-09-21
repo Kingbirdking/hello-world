aad2ab64f2bff19e2f0f8cf7394da14cf57d5ad7
Birdking <1260883860@qq.com>
2025-09-21 20:24:18 +0800

    Add files via upload

---
+++ 4.不同数制表示和转换/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" %d\t %x\t %o\n",29,29,29);
+    printf(" %d\t %x\t %o\n",025,025,025);
+    printf(" %d\t %x\t %o\n",0x1a,0x1a,0x1a);
+    printf(" %u\t %1d\n",47675u,742611);
+    return 0;
+}
