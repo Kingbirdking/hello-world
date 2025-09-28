c37a9ed9324497bcf36e625358781d285891ccd9
Birdking <1260883860@qq.com>
2025-09-28 13:30:36 +0800

    Add files via upload

---
+++ 9.三个数比较大小/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c,d;
+    printf("请输入三个数字（用逗号隔开）");
+    scanf("%f,%f,%f",&a,&b,&c);
+    if(b>a)
+    {
+    d=b;
+    b=a;
+    a=d;
+    }
+    if(c>a)
+    {
+    d=c;
+    c=a;
+    a=d;
+    }
+    if(b>c)
+    {d=b;
+    b=c;
+    c=d ;
+    }
+    printf("%f>%f>%f",a,b,c);
+    return 0;
+}
